a978652d9ede3c2758c0e08e1cd35e757364c77d
Tepigers <tommhall229@gmail.com>
2023-10-15 21:15:42 +0100

    added (forcful) loops

--- meowdio.c
+++ meowdio.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
 	ma_sound sound;
 
 	if (argc < 2){
-		printf("no file path <3");
+		printf("no file path <3\n");
 		return 69;
 	}
 
@@ -17,7 +17,8 @@ int main(int argc, char** argv) {
         return 69;
     }
 	
-	ma_engine_play_sound(&engine, argv[1], &sound);
+	ma_sound_init_from_file(&engine, argv[1], 0, NULL, NULL, &sound);
+	ma_sound_start(&sound);
 	ma_sound_set_looping(&sound, 1);
 
     printf("Press enter to quit...");
